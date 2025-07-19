4ce3911428d816aca50a2ffc86f0e71651be7418
LeonardoGB29 <lgaonab@unsa.edu.pe>
2025-07-19 18:31:05 -0500

    dejando referencias

--- src/main.cpp
+++ src/main.cpp
@@ -11,30 +11,47 @@ int main() {
     }
 
     Mat frame;
-    VisionProcessor vp;
+    VisionProcessor vp; // <----------- TAMBIEN ES NECESARIO
 
     while (true) {
         cap >> frame;
         if (frame.empty()) break;
 
+        // #################################################################################################
+        // ACCIONES NECESARIAS PARA DETECCIONES DE GESTOS
+        
         vp.processHand(frame);
         vp.update();
 
-        if (!vp.handDetected) {
+        if (!vp.handDetected) { //deteccion de mano
             putText(vp.outFrame, "No hand detected", Point(20, 60), FONT_HERSHEY_SIMPLEX, 0.8, Scalar(0,0,255), 2);
-        } else if (vp.isStop()) {
+        } 
+
+        else if (vp.isStop()) {
+            // colocar acciones 
             putText(vp.outFrame, "STOP", Point(20, 60), FONT_HERSHEY_SIMPLEX, 0.8, Scalar(0,0,255), 2);
-        } else if (vp.isAdvance()) {
+        } 
+
+        else if (vp.isAdvance()) { // w
+            // colocar acciones 
             putText(vp.outFrame, "ADVANCE (W)", Point(20, 60), FONT_HERSHEY_SIMPLEX, 0.8, Scalar(0,0,255), 2);
-        } else if (vp.isLeft()) {
+        } 
+
+        else if (vp.isLeft()) { // left
+            // colocar acciones 
             putText(vp.outFrame, "ADVANCE LEFT (A)", Point(20, 60), FONT_HERSHEY_SIMPLEX, 0.8, Scalar(0,0,255), 2);
-        } else if (vp.isRight()) {
+        } 
+
+        else if (vp.isRight()) { // right
+            // colocar acciones 
             putText(vp.outFrame, "ADVANCE RIGHT (D)", Point(20, 60), FONT_HERSHEY_SIMPLEX, 0.8, Scalar(0,0,255), 2);
         }
-
+        
         imshow("Camara Normal", frame);
         imshow("Vision Mano", vp.outFrame);
         imshow("HSV", vp.hsv);
+
+        // #################################################################################################
 
         char key = (char)waitKey(30);
         if (key == 27) break; // ESC para salir
