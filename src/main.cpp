673c34a613c13ade7260790a6272eb6cdd8a7adc
LeonardoGB29 <lgaonab@unsa.edu.pe>
2025-07-19 18:28:54 -0500

    modularizando acciones

--- src/main.cpp
+++ src/main.cpp
@@ -17,10 +17,21 @@ int main() {
         cap >> frame;
         if (frame.empty()) break;
 
-        // procesar mano
         vp.processHand(frame);
+        vp.update();
 
-        // Mostrar las ventanas
+        if (!vp.handDetected) {
+            putText(vp.outFrame, "No hand detected", Point(20, 60), FONT_HERSHEY_SIMPLEX, 0.8, Scalar(0,0,255), 2);
+        } else if (vp.isStop()) {
+            putText(vp.outFrame, "STOP", Point(20, 60), FONT_HERSHEY_SIMPLEX, 0.8, Scalar(0,0,255), 2);
+        } else if (vp.isAdvance()) {
+            putText(vp.outFrame, "ADVANCE (W)", Point(20, 60), FONT_HERSHEY_SIMPLEX, 0.8, Scalar(0,0,255), 2);
+        } else if (vp.isLeft()) {
+            putText(vp.outFrame, "ADVANCE LEFT (A)", Point(20, 60), FONT_HERSHEY_SIMPLEX, 0.8, Scalar(0,0,255), 2);
+        } else if (vp.isRight()) {
+            putText(vp.outFrame, "ADVANCE RIGHT (D)", Point(20, 60), FONT_HERSHEY_SIMPLEX, 0.8, Scalar(0,0,255), 2);
+        }
+
         imshow("Camara Normal", frame);
         imshow("Vision Mano", vp.outFrame);
         imshow("HSV", vp.hsv);
