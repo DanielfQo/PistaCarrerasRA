75d41fa4ab43c8831a1113b9174093ea592a6fd1
KevinRodriguezLima <krodriguezl@unsa.edu.pe>
2025-07-21 02:45:31 -0500

    Haciendo que el objeto se mueva según la rotación del patrón

--- src/test_glad.cpp
+++ src/test_glad.cpp
@@ -13,9 +13,33 @@
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
+glm::mat4 cvPoseToView(const cv::Mat& rvec, const cv::Mat& tvec){
+    cv::Mat R;
+    cv::Rodrigues(rvec, R);
+
+    cv::Mat view_cv = cv::Mat::eye(4, 4, CV_64F);
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j)
+            view_cv.at<double>(i, j) = R.at<double>(i, j);
+        view_cv.at<double>(i, 3) = tvec.at<double>(i);
+    }
+
+    cv::Mat cvToGl = (cv::Mat_<double>(4,4) <<
+         1,  0,  0, 0,
+         0, -1,  0, 0,
+         0,  0, -1, 0,
+         0,  0,  0, 1);
+    view_cv = cvToGl * view_cv;
+
+    glm::mat4 view(1.0f);
+    for (int r = 0; r < 4; ++r)
+        for (int c = 0; c < 4; ++c)
+            view[c][r] = static_cast<float>(view_cv.at<double>(r, c));
+    return view;
+}           
+
 int main() {
-    // Inicializar GLFW
-    if (!glfwInit()) {
+\    if (!glfwInit()) {
         std::cerr << "Error al inicializar GLFW\n";
         return -1;
     }
@@ -39,19 +63,11 @@ int main() {
 
     glEnable(GL_DEPTH_TEST);
 
-    // Crear el renderizador del modelo
     ModelRenderer renderer("models/perfumes.obj");
-
-    // Cámara fija
-    glm::mat4 view = glm::lookAt(glm::vec3(0.0f, 2.0f, 6.0f),
-                                 glm::vec3(0.0f, 0.0f, 0.0f),
-                                 glm::vec3(0.0f, 1.0f, 0.0f));
 
     glm::mat4 projection = glm::perspective(glm::radians(45.0f),
                                             (float)SCR_WIDTH / SCR_HEIGHT,
                                             0.1f, 100.0f);
-
-    renderer.SetViewProjection(view, projection);
 
     // Configurar cámara OpenCV
     cv::VideoCapture cap(0);
@@ -82,17 +98,19 @@ int main() {
         cap >> frame;
         if (frame.empty()) break;
 
-        procesarFrame(frame, K, dist, pose); // detección del marcador
+        procesarFrame(frame, K, dist, pose);
 
-        glClearColor(0.1f, 0.1f, 0.15f, 1.0f); // fondo oscuro
+        glClearColor(0.1f, 0.1f, 0.15f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         if (pose.poseValida) {
-            // Aplicar transformación base
-            glm::mat4 model = glm::scale(glm::mat4(1.0f), glm::vec3(0.2f)); // ajusta tamaño
+            glm::mat4 model = glm::scale(glm::mat4(1.0f), glm::vec3(1.0f));
+            glm::mat4 view = cvPoseToView(pose.rvec, pose.tvec);
+            renderer.SetViewProjection(view, projection);
             renderer.SetModelMatrix(model);
-            renderer.Draw(); // solo se dibuja si hay marcador
+            renderer.Draw();
         }
+
 
         glfwSwapBuffers(window);
         glfwPollEvents();
